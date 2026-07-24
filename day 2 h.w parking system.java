891de41c33c009f2e3a7d0482319b631117bbecf
harinibavani8-sudo <harinibavani8@gmail.com>
2026-07-24 09:23:10 +0530

    Update day 2 h.w parking system.java

--- day 2 h.w parking system.java
+++ day 2 h.w parking system.java
@@ -1,16 +1,16 @@
 class ParkingSystem {
-public:
-    ParkingSystem(int big, int medium, int small) {
-        
-    }
-    
-    bool addCar(int carType) {
-        
-    }
-};
 
-/**
- * Your ParkingSystem object will be instantiated and called as such:
- * ParkingSystem* obj = new ParkingSystem(big, medium, small);
- * bool param_1 = obj->addCar(carType);
- */
+    private int[] spots;
+
+    public ParkingSystem(int big, int medium, int small) {
+        spots = new int[]{big, medium, small};
+    }
+
+    public boolean addCar(int carType) {
+        if (spots[carType - 1] > 0) {
+            spots[carType - 1]--;
+            return true;
+        }
+        return false;
+    }
+}
