610e634117ae392cc118d6a01bb17ece354a76a0
harinibavani8-sudo <harinibavani8@gmail.com>
2026-07-26 10:22:54 +0530

    Update Lab -1a - parking system.java

--- Lab -1a - parking system.java
+++ Lab -1a - parking system.java
@@ -1,16 +1,30 @@
-class ParkingSystem {
+class ParkingSystem { 
+    private int b = 0,m = 0,s = 0; 
+    int bigcount = 0,medcount = 0,smallcount = 0; 
+    public ParkingSystem(int big, int medium, int small) { 
+        this.b = big; 
+        this.m = medium; 
+        this.s = small; 
+    } 
+    public boolean addCar(int carType) { 
+        
+         if(carType == 1){ 
+            bigcount+=1; 
+            boolean check = bigcount<=b ? true:false; 
+            return check; 
+         } 
+            if(carType == 2){ 
+                medcount+=1; 
+                boolean check = medcount<=m ? true:false; 
+            return check; 
+            } 
+            if(carType == 3){ 
+                smallcount+=1; 
+                boolean check = smallcount<=s ? true:false; 
+            return check; 
+            } 
+            return true; 
+    } 
+    
 
-    private int[] spots;
-
-    public ParkingSystem(int big, int medium, int small) {
-        spots = new int[]{big, medium, small};
-    }
-
-    public boolean addCar(int carType) {
-        if (spots[carType - 1] > 0) {
-            spots[carType - 1]--;
-            return true;
-        }
-        return false;
-    }
 }
